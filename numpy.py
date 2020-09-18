61c54629b80c835639ce87e0ffb639f0ac3bb584
Aniket Naik <naikaniket26@gmail.com>
2020-09-18 22:25:56 +0530

    Practice numpy

---
+++ numpy.py
@@ -0,0 +1,11 @@
+#Create a Null vector (all zeros) of size 10 and set in variable 'Z'
+
+Z = np.zeros(10)
+
+#Create a 1D array of numbers from 0 to 9 and set it in a var called "arr"
+
+arr = np.arange(10)
+
+#Create a 3*3*3 array with random values and set it in variable called "arr"
+
+arr = np.random((3,3,3))
