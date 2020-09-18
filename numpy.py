8730a7b41da2d543c9691623cab162eb6863773c
Aniket Naik <naikaniket26@gmail.com>
2020-09-18 23:35:29 +0530

    Numpy quiz ans practice

--- numpy.py
+++ numpy.py
@@ -18,3 +18,15 @@ max_val = arr4.max()
 
 #First create a 1D array with numbers from 1 to 9 and then convert it into a 3x3 grid. Store the final answer in the variable called “grid”.
 grid = np.arange(1,10).reshape((3,3))
+
+#Replace the max value in given vector  "arr6" with -1
+arr6 = np.arange(10)
+arr6[arr6.argmax()] = -1
+
+#Reverse teh rows of 2D array arr7
+arr7= np.arange(9).reshape(3,3)
+arr7
+
+#Subtract the mean row of 2D array 'arr8' and rename it to transformed_arr8
+arr8 = np.random(3,10)
+transformed_arr8 = arr8 - arr8.mean(axis=1,keepdims=True)
